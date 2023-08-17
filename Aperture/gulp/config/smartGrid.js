8fc2226b2dccd7330087e8c1ea02c4240b6d2c0c
Bermihrom <funnyhak876@gmail.com>
2023-08-17 12:59:11 +0300

    I changed the structure of the project. All working.

---
+++ Aperture/gulp/config/smartGrid.js
@@ -0,0 +1,32 @@
+import smartGrid from "smart-grid"
+
+const settings = {
+    columns: 12,
+    offset: '15px',
+    wrapper: {
+        maxWidth: '960px',
+        fields: '30px',
+    },
+    breakPoints: {
+        md: {
+            width: "992px",
+            fields: "20px"
+        },
+        sm: {
+            width: "720px",
+            fields: "10px"
+        },
+        xs: {
+            width: "576px",
+            fields: "5px"
+        },
+        xss: {
+            width: "380px",
+            fields: "5px"
+        },
+    },
+    oldSizeStyle: false,
+    properties: []
+};
+
+smartGrid(`../../src/smart`, settings);
